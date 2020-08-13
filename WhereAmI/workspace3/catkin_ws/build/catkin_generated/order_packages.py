a7402dbfed0cd03c9fa8c7f431c0cc22d6ed6db6
krithika <kritikagovindaraj@github.com>
2020-08-12 19:59:22 -0500

    robot_files

--- WhereAmI/workspace3/catkin_ws/build/catkin_generated/order_packages.py
+++ WhereAmI/workspace3/catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/krithika/Documents/RoboticsSoftwareNanodegree/WhereAmI/workspace3/catkin_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/home/krithika/Documents/RoboticsSoftwareNanodegree/WhereAmI/workspace3/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/krithika/Documents/RoboticsSoftwareNanodegree/WhereAmI/workspace3/catkin_ws/devel;/opt/ros/kinetic" != "" else []
